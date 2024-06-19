75f7415fc4da9958bc0aa2ae0df9587f11274940
l00duck <nebojsanikolic1997@gmail.com>
2024-06-19 11:00:16 +0200

    Movie app project utilizing OMDB API.

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const URL_API = "http://www.omdbapi.com?apikey=4aa90c76";
+
+const movie1 = {
+  Title: "Spiderman the Verse",
+  Year: "2019–",
+  imdbID: "tt12122034",
+  Type: "series",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg",
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${URL_API}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for your movies here"
+          value={searchTerm}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
